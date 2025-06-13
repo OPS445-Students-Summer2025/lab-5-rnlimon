cbfb6a8e42f887056db7c10e9a9c4b2dd8733aff
Raffy Limon <rnlimon@myseneca.ca>
2025-06-13 08:08:37 -0400

    Added lab5a.py

---
+++ lab5a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Author ID: rnlimon
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    f = open(file_name,'r')
+    data = f.read()
+    f.close()
+    return data
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name,
+    f = open(file_name, 'r')
+    lines_new = list(f)
+
+    # return its entire contents as a list of lines without new-line characters
+    lines = []
+    for line in lines_new:
+        lines.append(line.strip())
+    f.close
+    return lines
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
